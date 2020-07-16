05a0286391a0d641afd632464dd43d42ed1f74e8
jorgenavarrom <j.navarro@wi.knaw.nl>
2020-07-16 18:08:52 +0200

    BGClib:Metabolites: added more fields

--- BGClib/BGClib.py
+++ BGClib/BGClib.py
@@ -2799,9 +2799,15 @@ class Organism:
 
 class Metabolite:
     def __init__(self):
-        self.name = ""                  # Main name
-        self.alias = []                 # Alternative names
+        self.name = ""                  # e.g. "1,3,6,8-tetrahydroxynaphthalene"
+        self.alias = []                 # Alternative names e.g. "THN"
         self.database = {}              # Key=database (PubChem, 
                                         #  ChemSpider, etc.). Item=id
         self.SMILES = ""
-        self.compound_family = ""       # semi-arbitrary e.g. "azaphilone"
+        self.compound_family = ""       # semi-arbitrary e.g. "azaphilone
+        self.role = ""                  # defense, communication etc.
+        self.properties = []            # e.g. "volatile"
+        
+        self.characterized = False      # toggle to True only if supported by 
+                                        # publication
+        self.comment = ""
